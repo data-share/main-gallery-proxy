ec97f34ef70c4f1a986aea818e029c3912065641
acolascione <acolascione27@gmail.com>
2020-10-30 23:18:13 +0000

    proxy running on ec2 instance, updated other services with deployed ip addresses

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 // const router = express.Router();
 // const morgan = require('morgan');
 const path = require('path');
@@ -6,77 +7,105 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3005;
 
+app.use(compression());
 // app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../public')));
 
 // Bookings
+// Bookings
 app.get('/:restaurantId', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3000/${reqProxy.params.restaurantId}`)
+  axios.get(`http://13.57.236.176/${reqProxy.params.restaurantId}`)
     .then((response) => {
       resProxy.status(200).send(response.data)
     });
 });
 
 app.get('/api/bookings/restaurantName/:restaurantId', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3000/api/bookings/restaurantName/${reqProxy.params.restaurantId}`)
+  axios.get(`http://13.57.236.176/api/bookings/restaurantName/${reqProxy.params.restaurantId}`)
     .then((response) => {
       resProxy.status(200).send(response.data)
-    });
+    })
+   .catch((err) => {
+      resProxy.status(400).send(err)
+    })
 });
 
 app.get('/api/bookings/:restaurantId', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3000/api/bookings/${reqProxy.params.restaurantId}`)
+  axios.get(`http://13.57.236.176/api/bookings/${reqProxy.params.restaurantId}`)
     .then((response) => {
       resProxy.status(200).send(response.data)
-    });
+    })
+    .catch((err) => {
+      resProxy.status(400).send(err)
+    })
 });
 
 app.post('/api/bookings/:restaurantId', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3000/api/bookings/${reqProxy.params.restaurantId}`, {
+  axios.get(`http://13.57.236.176/api/bookings/${reqProxy.params.restaurantId}`, {
     params: reqProxy.query
   })
-    .then((response) => {
-      resProxy.status(200).send(response.data)
-    });
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  })
+  .catch((err) => {
+    resProxy.status(400).send(err)
+  })
 });
 
 //Photos
 
-app.get('/api/restaurants/photos/:id', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3003/api/restaurants/photos/${reqProxy.params.id}`)
-    .then((response) => {
-      resProxy.status(200).send(response.data)
-    });
+app.get('/api/restaurants/:id/photos', (reqProxy, resProxy) => {
+axios.get(`http://3.86.33.200/api/restaurants/${reqProxy.params.id}/photos`)
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  })
+  .catch((err) => {
+    resProxy.status(400).send(err)
+  })
 });
 
 //Popular Dishes
-app.get('/api/dishes/restaurant/:id', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3001/api/dishes/restaurant/${reqProxy.params.id}`)
-    .then((response) => {
-      resProxy.status(200).send(response.data)
-    });
+app.get('/api/restaurant/:id', (reqProxy, resProxy) => {
+axios.get(`http://52.53.226.190/api/restaurant/${reqProxy.params.id}`)
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  })
+	.catch((err) => {
+    resProxy.status(400).send(err)
+  })
+
 });
 
+app.get('/api/dish/:id', (reqProxy, resProxy) => {
+axios.get(`http://52.53.226.190/api/restaurant/${reqProxy.params.id}`)
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  })
+	.catch((err) => {
+    resProxy.status(400).send(err)
+  })
+
+});
+	
 //Reviews
-app.get('/api/restaurants/:id', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3002/api/restaurants/${reqProxy.params.id}`)
-    .then((response) => {
-      resProxy.status(200).send(response.data)
-    });
+app.get('/api/restaurants/:id/reviews', (reqProxy, resProxy) => {
+axios.get(`http://localhost/api/restaurants/${reqProxy.params.id}`)
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  });
 });
 
-app.get('/api/review_list/:id', (reqProxy, resProxy) => {
-  axios.get(`http://localhost:3002/api/review_list/${reqProxy.params.id}`)
-    .then((response) => {
-      resProxy.status(200).send(response.data)
-    });
+app.get('/api/restaurants/:id/reviews', (reqProxy, resProxy) => {
+axios.get(`http://localhost:3002/api/restaurants/:id/reviews/${reqProxy.params.id}`)
+  .then((response) => {
+    resProxy.status(200).send(response.data)
+  });
 });
-
 
 app.listen(port, () => {
-  console.log(`server running on http://localhost:${port}`);
-});
+    console.log(`server running on http://localhost:${port}`);
+  });
